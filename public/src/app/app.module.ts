a148474804f0ee053a23f7cf34faa2084da13ead
BryantCR <bryancvargas20@gmail.com>
2021-11-30 10:09:16 -0600

    all working good

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'; //for API's and more
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { FormsModule } from '@angular/forms';
+import { UserguiComponent } from './usergui/usergui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    UserguiComponent
   ],
   imports: [
     BrowserModule,
